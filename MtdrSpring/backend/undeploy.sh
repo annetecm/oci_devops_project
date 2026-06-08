995b8079e4b5181e372acdf0773183793447ac70
Anett <anettmava@gmail.com>
2026-06-08 09:38:05 -0600

    Add --ignore-not-found flag to undeploy commands

--- MtdrSpring/backend/undeploy.sh
+++ MtdrSpring/backend/undeploy.sh
@@ -1,3 +1,3 @@
 echo delete frontend deployment and service...
-kubectl -n mtdrworkshop delete deployment todolistapp-springboot-deployment
-kubectl -n mtdrworkshop delete service todolistapp-springboot-service
+kubectl -n mtdrworkshop delete deployment todolistapp-springboot-deployment --ignore-not-found=true
+kubectl -n mtdrworkshop delete service todolistapp-springboot-service --ignore-not-found=true
